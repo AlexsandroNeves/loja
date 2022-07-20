4adc404d188aa790101706a4d5f4e651c47c1d4d
Alexsandro Neves <alexsandrodnss@gmail.com>
2022-07-20 01:35:01 -0300

    ajuste de conflito

--- src/Components/tipbar/Tipbar.jsx
+++ src/Components/tipbar/Tipbar.jsx
@@ -1,11 +1,9 @@
 import { ContainerTipBar } from "../../ComponentsStyles/Components"
-<<<<<<< HEAD
-import '../tipbar/tipbar.css'
-import img01 from './img/Tipbar01.png'
 import imgEstoque from './img/estoque.png'
 import imgTroca from './img/troca.png'
 import imgGanhe from './img/ganhe.png'
 import imgFrete from './img/frete.png'
+import '../tipbar/tipbar.css' 
 const Tipbar = () => {
     return (
         <>
@@ -48,62 +46,6 @@ const Tipbar = () => {
                         <p className="description--tipbar">Em compras acima de R$ 499,00</p>
                     </div>
                 </ContainerTipBar>
-=======
-import img01 from './img/Tipbar01.png'
-const Tipbar = () => {
-    return (
-        <>
-            <h5>Por que comprar na Maeztra?</h5>
-            <div>
-
-            <ContainerTipBar>
-                <img src={img01} alt="" />
-                <div>
-                    <h4>
-                        Produtos importados
-                    </h4>
-                    <p className="">Produto de Alta Qualidade</p>
-                </div>
-            </ContainerTipBar>
-
-            <ContainerTipBar>
-                <img src={img01} alt="" />
-                <div>
-                    <h4>
-                        Estoque no Brazil
-                    </h4>
-                    <p className="">Produto de Alta Qualidade</p>
-                </div>
-            </ContainerTipBar>
-
-            <ContainerTipBar>
-                <img src={img01} alt="" />
-                <div>
-                    <h4>
-                        Trocas Garantidas
-                    </h4>
-                    <p className="">Produto de Alta Qualidade</p>
-                </div>
-            </ContainerTipBar>
-            <ContainerTipBar>
-                <img src={img01} alt="" />
-                <div>
-                    <h4>
-                        Ganhe 5% off
-                    </h4>
-                    <p className="">Produto de Alta Qualidade</p>
-                </div>
-            </ContainerTipBar>
-            <ContainerTipBar>
-                <img src={img01} alt="" />
-                <div>
-                    <h4>
-                        Frete Grátis
-                    </h4>
-                    <p className="">Produto de Alta Qualidade</p>
-                </div>
-            </ContainerTipBar>
->>>>>>> 7aedf13b8f633a65f7252448499775154812f80d
 
             </div>
         </>
